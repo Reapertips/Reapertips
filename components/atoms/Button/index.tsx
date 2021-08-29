9030b61144b4e59f2eefab5b85ca1462e2b66aed
Davide Ciulla <davide.ciulla@hotmail.com>
2021-08-29 11:30:19 +0200

    Finished button component

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -1,42 +1,52 @@
 import Link from 'next/link';
 import { StyledButton } from './Button.styles';
 import { button__blue, button__red } from '@utils/colors';
-
-export interface IButton {
-	title: string;
-	subtitle?: string;
-	color?: 'red' | 'blue';
-	type?: 'primary' | 'secondary';
-	external?: boolean;
-	link?: string;
-	onClick?: () => void;
-}
+import { IButton } from '@utils/types';
 
 export const Button = ({
 	title,
 	subtitle,
 	color = 'blue',
 	type = 'primary',
-	external = false,
+	externalLink = false,
 	link,
 	...props
 }: IButton) => {
 	const ButtonColor = color === 'blue' ? button__blue : button__red;
-	const target = link !== undefined && external === true ? '_blank' : '';
 
-	return link !== undefined ? (
-		<Link href={link}>
-			<a target={target}>
-				<StyledButton className="button" color={ButtonColor} mode={type}>
+	if (link === undefined) {
+		return (
+			<StyledButton color={ButtonColor} mode={type}>
+				<h1>{title}</h1>
+				<p>{subtitle}</p>
+			</StyledButton>
+		);
+	} else {
+		if (!externalLink) {
+			const getPageLink = () => {
+				const dotSep = link.split('.');
+				const lastDotPart = dotSep[dotSep.length - 1];
+				const paths = lastDotPart.split('/').slice(1);
+				return paths.join('/');
+			};
+
+			return (
+				<Link href={getPageLink()}>
+					<a>
+						<StyledButton color={ButtonColor} mode={type}>
+							<h1>{title}</h1>
+							<p>{subtitle}</p>
+						</StyledButton>
+					</a>
+				</Link>
+			);
+		} else {
+			return (
+				<StyledButton as="a" color={ButtonColor} mode={type} href={link} target="_blank" rel="noopener noreferrer">
 					<h1>{title}</h1>
 					<p>{subtitle}</p>
 				</StyledButton>
-			</a>
-		</Link>
-	) : (
-		<StyledButton className="button" color={ButtonColor} mode={type}>
-			<h1>{title}</h1>
-			<p>{subtitle}</p>
-		</StyledButton>
-	);
+			);
+		}
+	}
 };
