9f356782bfc19d0ee3acf9886605bb49f913d518
Davide Ciulla <davide.ciulla@hotmail.com>
2021-08-29 17:45:52 +0200

    Improved downloads page

--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -1,15 +1,14 @@
-import Image from 'next/image';
 import styled from 'styled-components';
 import { request } from 'graphql-request';
 
 import { Post } from '@components/atoms/Post';
 import { Navbar } from '@components/atoms/Navbar';
+import { TopMessage } from '@components/atoms/TopMessage';
 import Layout from '@components/templates/Layout';
 import Container from '@components/templates/Container';
 
 import { cms, getDownloadsPage } from '@utils/queries';
 import type { IDownloads } from '@utils/types';
-import { blue, white } from '@utils/colors';
 
 const Gallery = styled.div`
 	display: grid;
@@ -22,28 +21,12 @@ const StyledPost = styled(Post)`
 	aspect-ratio: 1/1;
 `;
 
-const Message = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	gap: 0.5rem;
-	background: ${blue};
-	color: ${white};
-	width: 100%;
-	border-radius: 12px;
-	padding: 0.5rem 1rem;
-	margin-bottom: 1.5rem;
-`;
-
-const Downloads = ({ pageTitle, pageDescription, seo, posts }: IDownloads) => {
+const Downloads = ({ pageTitle, pageDescription, seo, topMessageIcon, topMessageText, posts }: IDownloads) => {
 	return (
-		<Layout pageTitle={pageTitle} pageDescription={pageDescription}>
+		<Layout pageTitle={pageTitle} pageDescription={pageDescription} ogImage={seo.ogImage.url}>
 			<Navbar pageTitle={pageTitle} />
 			<Container>
-				<Message>
-					<Image src="/icons/click.svg" alt="White cursor icon" width={32} height={32} />
-					<p>Click on the related post to get the files!</p>
-				</Message>
+				<TopMessage icon={topMessageIcon} text={topMessageText} />
 
 				<Gallery>
 					{posts.map((post) => (
