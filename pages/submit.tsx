233f543cbc5264372562623c937b9d88a735a16e
Davide Ciulla <davide.ciulla@hotmail.com>
2021-08-30 16:44:18 +0200

    Improved submit page

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { request } from 'graphql-request';
+import { SuccessToast, ErrorToast } from '@lib/toast';
 
 import { TopMessage } from '@components/atoms/TopMessage';
-import { FormTextInput } from '@components/atoms/FormTextInput';
-import { FormRadioGroup } from '@components/atoms/FormRadioGroup';
+import { TextInput } from '@FormElements/TextInput';
+import { SubmitButton } from '@FormElements/SubmitButton';
+import { RadioGroup } from '@FormElements/RadioGroup';
 import { Navbar } from '@components/organisms/Navbar';
 import Layout from '@components/templates/Layout';
 import Container from '@components/templates/Container';
@@ -30,6 +33,46 @@ const Submit = ({
 	creditsOptions,
 	submitButton
 }: ISubmit) => {
+	const [message, setMessage] = useState<string>('');
+	const [username, setUsername] = useState<string>('');
+	const [credits, setCredits] = useState<string>(creditsOptions[0]);
+	const [submitText, setSubmitText] = useState<string>(submitButton);
+
+	const resetForm = () => {
+		setMessage('');
+		setUsername('');
+		setCredits(creditsOptions[0]);
+	};
+
+	const handleSubmit = async (e: { preventDefault: () => void; currentTarget: { elements: any } }) => {
+		e.preventDefault();
+
+		setSubmitText('Submitting...');
+
+		const formElements = [...e.currentTarget.elements];
+
+		const filledOutElements = formElements
+			.filter((elem) => !!elem.value)
+			.map((element) => encodeURIComponent(element.name) + '=' + encodeURIComponent(element.value))
+			.join('&');
+
+		await fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: filledOutElements
+		})
+			.then(() => {
+				SuccessToast('Tip sent!');
+				setTimeout(() => {
+					setSubmitText(submitButton);
+					resetForm();
+				}, 2000);
+			})
+			.catch((_) => {
+				ErrorToast('Something went wrong');
+			});
+	};
+
 	return (
 		<Layout pageTitle={pageTitle} pageDescription={pageDescription} ogImage={seo.ogImage.url}>
 			<Navbar pageTitle={pageTitle} />
@@ -43,22 +86,30 @@ const Submit = ({
 					<input type="radio" name="credits" id="No, it's okay." />
 				</form>
 
-				<form name="tips-suggestions" method="post">
+				<form name="tips-suggestions" method="post" onSubmit={handleSubmit}>
 					<input type="hidden" name="form-name" value="tips-suggestions" />
-					<FormTextInput
+					<TextInput
 						name="message"
 						label={{ text: tipLabel, size: 'big' }}
 						description={tipDescription}
-						textarea={{ placeholder: tipPlaceholder }}
+						textarea={{ placeholder: tipPlaceholder, value: message }}
+						callback={setMessage}
 						required
 					/>
-					<FormTextInput
+					<TextInput
 						name="username"
 						label={{ text: usernameLabel, size: 'default' }}
-						input={{ type: 'text', placeholder: usernamePlaceholder }}
+						input={{ type: 'text', placeholder: usernamePlaceholder, value: username }}
+						callback={setUsername}
 					/>
-					<FormRadioGroup title={creditsLabel} name="credits" options={creditsOptions} />
-					<FormTextInput name="submit" input={{ type: 'submit', value: submitButton }} required />
+					<RadioGroup
+						title={creditsLabel}
+						name="credits"
+						options={creditsOptions}
+						selected={credits}
+						callback={setCredits}
+					/>
+					<SubmitButton value={submitText} />
 				</form>
 			</Container>
 		</Layout>
