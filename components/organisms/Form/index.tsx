f8bc25f9542fa7136735e5ae70ccf21e93f75224
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-01 17:06:12 +0200

    Moved form to a separate component

---
+++ components/organisms/Form/index.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+
+import { TextInput } from '@FormElements/TextInput';
+import { SubmitButton } from '@FormElements/SubmitButton';
+import { RadioGroup } from '@FormElements/RadioGroup';
+
+import type { ISubmit } from '@utils/types';
+import { SuccessToast, ErrorToast } from '@lib/toast';
+
+export const Form = ({
+	tipLabel,
+	tipDescription,
+	tipPlaceholder,
+	usernameLabel,
+	usernamePlaceholder,
+	creditsLabel,
+	creditsOptions,
+	submitButton
+}: ISubmit) => {
+	const [message, setMessage] = useState<string>('');
+	const [username, setUsername] = useState<string>('');
+	const [credits, setCredits] = useState<string>(creditsOptions[0]);
+	const [submitText, setSubmitText] = useState<string>(submitButton);
+
+	const resetForm = () => {
+		setMessage('');
+		setUsername('');
+		setCredits(creditsOptions[0]);
+	};
+
+	const handleSubmit = async (e: { preventDefault: () => void; currentTarget: { elements: any } }) => {
+		e.preventDefault();
+
+		setSubmitText('Submitting...');
+
+		const formElements = [...e.currentTarget.elements];
+
+		const filledOutElements = formElements
+			.filter((elem) => !!elem.value)
+			.map((element) => encodeURIComponent(element.name) + '=' + encodeURIComponent(element.value))
+			.join('&');
+
+		await fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: filledOutElements
+		})
+			.then(() => {
+				SuccessToast('Tip sent!');
+				setTimeout(() => {
+					setSubmitText(submitButton);
+					resetForm();
+				}, 2000);
+			})
+			.catch((_) => {
+				ErrorToast('Something went wrong');
+			});
+	};
+
+	return (
+		<form name="reapertips-suggestions" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+			<input type="hidden" name="form-name" value="reapertips-suggestions" />
+			<TextInput
+				name="message"
+				label={{ text: tipLabel, size: 'big' }}
+				description={tipDescription}
+				textarea={{ placeholder: tipPlaceholder, value: message }}
+				callback={setMessage}
+				required
+			/>
+			<TextInput
+				name="username"
+				label={{ text: usernameLabel, size: 'default' }}
+				input={{ type: 'text', placeholder: usernamePlaceholder, value: username }}
+				callback={setUsername}
+			/>
+			<RadioGroup
+				title={creditsLabel}
+				name="credits"
+				options={creditsOptions}
+				selected={credits}
+				callback={setCredits}
+			/>
+			<SubmitButton value={submitText} />
+		</form>
+	);
+};
--- pages/submit.tsx
+++ pages/submit.tsx
@@ -1,109 +1,27 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import { request } from 'graphql-request';
-import { SuccessToast, ErrorToast } from '@lib/toast';
 
 import { TopMessage } from '@components/atoms/TopMessage';
-import { TextInput } from '@FormElements/TextInput';
-import { SubmitButton } from '@FormElements/SubmitButton';
-import { RadioGroup } from '@FormElements/RadioGroup';
 import { Navbar } from '@components/organisms/Navbar';
+import { Form } from '@components/organisms/Form';
 import Layout from '@components/templates/Layout';
 import Container from '@components/templates/Container';
 
-import { cms, getSubmitPage } from '@utils/queries';
+import { fetch, getSubmitPage } from '@utils/queries';
 import type { ISubmit } from '@utils/types';
 
 const StyledTopMessage = styled(TopMessage)`
 	margin-bottom: 2.5rem;
 `;
 
-const Submit = ({
-	pageTitle,
-	pageDescription,
-	seo,
-	topMessageIcon,
-	topMessageText,
-	tipLabel,
-	tipDescription,
-	tipPlaceholder,
-	usernameLabel,
-	usernamePlaceholder,
-	creditsLabel,
-	creditsOptions,
-	submitButton
-}: ISubmit) => {
-	const [message, setMessage] = useState<string>('');
-	const [username, setUsername] = useState<string>('');
-	const [credits, setCredits] = useState<string>(creditsOptions[0]);
-	const [submitText, setSubmitText] = useState<string>(submitButton);
-
-	const resetForm = () => {
-		setMessage('');
-		setUsername('');
-		setCredits(creditsOptions[0]);
-	};
-
-	const handleSubmit = async (e: { preventDefault: () => void; currentTarget: { elements: any } }) => {
-		e.preventDefault();
-
-		setSubmitText('Submitting...');
-
-		const formElements = [...e.currentTarget.elements];
-
-		const filledOutElements = formElements
-			.filter((elem) => !!elem.value)
-			.map((element) => encodeURIComponent(element.name) + '=' + encodeURIComponent(element.value))
-			.join('&');
-
-		await fetch('/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: filledOutElements
-		})
-			.then(() => {
-				SuccessToast('Tip sent!');
-				setTimeout(() => {
-					setSubmitText(submitButton);
-					resetForm();
-				}, 2000);
-			})
-			.catch((_) => {
-				ErrorToast('Something went wrong');
-			});
-	};
+const Submit = (props: ISubmit) => {
+	const { pageTitle, pageDescription, seo, topMessageIcon, topMessageText } = props;
 
 	return (
 		<Layout pageTitle={pageTitle} pageDescription={pageDescription} ogImage={seo.ogImage.url}>
 			<Navbar pageTitle={pageTitle} />
 			<Container>
 				<StyledTopMessage icon={topMessageIcon} text={topMessageText} closable={true} />
-
-				<form name="reapertips-suggestions" method="POST" data-netlify="true" onSubmit={handleSubmit}>
-					<input type="hidden" name="form-name" value="reapertips-suggestions" />
-					<TextInput
-						name="message"
-						label={{ text: tipLabel, size: 'big' }}
-						description={tipDescription}
-						textarea={{ placeholder: tipPlaceholder, value: message }}
-						callback={setMessage}
-						required
-					/>
-					<TextInput
-						name="username"
-						label={{ text: usernameLabel, size: 'default' }}
-						input={{ type: 'text', placeholder: usernamePlaceholder, value: username }}
-						callback={setUsername}
-					/>
-					<RadioGroup
-						title={creditsLabel}
-						name="credits"
-						options={creditsOptions}
-						selected={credits}
-						callback={setCredits}
-					/>
-					<SubmitButton value={submitText} />
-				</form>
+				<Form {...props} />
 			</Container>
 		</Layout>
 	);
@@ -112,7 +30,7 @@ const Submit = ({
 export default Submit;
 
 export const getStaticProps = async () => {
-	const data = (await request(cms, getSubmitPage)).submitPages[0];
+	const data = (await fetch(getSubmitPage)).submitPages[0];
 
 	return {
 		props: data
