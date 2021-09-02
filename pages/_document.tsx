752f5dfb519f09c067fd715f4dfc13039d9e5278
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-02 21:09:26 +0200

    Got rid of the usual styled-component error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,22 @@
-import Document, { DocumentContext, Html } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					// eslint-disable-next-line react/display-name
-					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-				});
-
-			const initialProps = await Document.getInitialProps(ctx);
-			return {
-				...initialProps,
-				styles: (
-					<>
-						<Html lang="en" />
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				)
-			};
-		} finally {
-			sheet.seal();
-		}
+	render() {
+		return (
+			<Html lang="en">
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
+
+export default MyDocument;
