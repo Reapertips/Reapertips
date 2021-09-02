ac4d72349268b9cd7a5bde4f6021d90e9f8819c1
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-02 15:39:38 +0200

    Wrote button stories

--- components/atoms/Button/ButtonCore.stories.tsx
+++ components/atoms/Button/ButtonCore.stories.tsx
@@ -9,18 +9,58 @@ export default {
 
 const Template: Story<IButtonCore> = (args) => <ButtonCore {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const PrimaryBlue = Template.bind({});
+PrimaryBlue.args = {
 	title: 'Title',
 	subtitle: 'Subtitle',
 	color: 'blue',
 	mode: 'primary'
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
+export const PrimaryBlueIcon = Template.bind({});
+PrimaryBlueIcon.args = {
 	title: 'Title',
 	subtitle: 'Subtitle',
 	color: 'blue',
+	mode: 'primary',
+	icon: {
+		url: 'https://media.graphcms.com/FGasKszQRb23cgWqUVpg',
+		alt: 'Kofi logo'
+	}
+};
+
+export const PrimaryRed = Template.bind({});
+PrimaryRed.args = {
+	title: 'Title',
+	subtitle: 'Subtitle',
+	color: 'red',
+	mode: 'primary'
+};
+
+export const PrimaryRedIcon = Template.bind({});
+PrimaryRedIcon.args = {
+	title: 'Title',
+	subtitle: 'Subtitle',
+	color: 'red',
+	mode: 'primary',
+	icon: {
+		url: 'https://media.graphcms.com/FGasKszQRb23cgWqUVpg',
+		alt: 'Kofi logo'
+	}
+};
+
+export const SecondaryBlue = Template.bind({});
+SecondaryBlue.args = {
+	title: 'Title',
+	subtitle: 'Subtitle',
+	color: 'blue',
+	mode: 'secondary'
+};
+
+export const SecondaryRed = Template.bind({});
+SecondaryRed.args = {
+	title: 'Title',
+	subtitle: 'Subtitle',
+	color: 'red',
 	mode: 'secondary'
 };
