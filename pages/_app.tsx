53f347c59f17873a609f657976cc7c51b1f3ac33
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-04 23:03:27 +0200

    Imported Open Sans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
-
-import '@fontsource/ubuntu';
-
 import { ToastContainer } from 'react-toastify';
 import { toastConfig } from '@lib/toast';
 import 'react-toastify/dist/ReactToastify.css';
+
+import '@fontsource/ubuntu';
+import '@fontsource/ubuntu/700.css';
+
+import '@fontsource/open-sans';
+import '@fontsource/open-sans/700.css';
 
 import '../styles/globals.css';
 
