46afb1a789cd861a86c5150440dbe4e00fb17f03
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-04 19:33:46 +0200

    Debugging flash of unstyled content

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,47 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
-	render() {
-		return (
-			<Html lang="en">
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					// eslint-disable-next-line react/display-name
+					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				)
+			};
+		} finally {
+			sheet.seal();
+		}
+
+		// 	const initialProps = await Document.getInitialProps(ctx);
+		// 	return { ...initialProps };
+		// }
+
+		// render() {
+		// 	return (
+		// 		<Html lang="en">
+		// 			<Head />
+		// 			<body>
+		// 				<Main />
+		// 				<NextScript />
+		// 			</body>
+		// 		</Html>
+		// 	);
 	}
 }
 
-export default MyDocument;
+// export default MyDocument;
