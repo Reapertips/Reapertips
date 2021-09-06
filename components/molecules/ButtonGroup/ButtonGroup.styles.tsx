67b06ae62865aa6a7dca21df6397d0e2aa53ee02
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-06 19:42:47 +0200

    Fixed buttons group max-width

--- components/molecules/ButtonGroup/ButtonGroup.styles.tsx
+++ components/molecules/ButtonGroup/ButtonGroup.styles.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
+import { sizes } from '@utils/mediaQueries';
+
 export const StyledButtonGroup = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
 	width: 100%;
+	max-width: ${sizes.xxs};
 
 	& .button {
 		margin: 0 auto;
