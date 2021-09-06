08e161fea15a817c73422b3158ea9a56dabf5aa4
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-06 21:55:39 +0200

    Debugging form not submitting

--- components/organisms/Form/index.tsx
+++ components/organisms/Form/index.tsx
@@ -46,7 +46,7 @@ export const Form = ({
 
 		const filledOutElements = formElements
 			.filter((elem) => !!elem.value)
-			.map((element) => encodeURIComponent(decode(element.name)) + '=' + encodeURIComponent(decode(element.value)))
+			.map((element) => encodeURIComponent(element.name) + '=' + encodeURIComponent(decode(element.value)))
 			.join('&');
 
 		console.log(filledOutElements);
