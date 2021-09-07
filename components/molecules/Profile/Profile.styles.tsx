a12e51ab50f2244e6bf91ddbf224307001cbacef
Davide Ciulla <davide.ciulla@hotmail.com>
2021-09-07 02:30:16 +0200

    Using instagram logo in profile component

--- components/molecules/Profile/Profile.styles.tsx
+++ components/molecules/Profile/Profile.styles.tsx
@@ -8,7 +8,9 @@ export const StyledProfile = styled.div`
 	gap: 1rem;
 	color: ${white};
 	margin: 3rem 0 2.5rem 0;
+`;
 
+export const ProfilePicture = styled.div`
 	img {
 		height: 150px;
 		border-radius: 100%;
@@ -16,7 +18,14 @@ export const StyledProfile = styled.div`
 `;
 
 export const Username = styled.a`
+	display: flex;
+	align-items: center;
 	font-size: 2rem;
 	font-family: 'Ubuntu', sans-serif;
 	font-weight: 700;
+
+	p {
+		display: inline;
+		margin-left: 0.5rem;
+	}
 `;
--- components/molecules/Profile/index.tsx
+++ components/molecules/Profile/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 import { IImage } from '@utils/types';
-import { StyledProfile, Username } from './Profile.styles';
+import { StyledProfile, ProfilePicture, Username } from './Profile.styles';
 
 export interface IProfile {
 	username: string;
@@ -11,9 +11,12 @@ export interface IProfile {
 export const Profile = ({ username, photo }: IProfile) => {
 	return (
 		<StyledProfile>
-			<Image src={photo.url} alt={photo.alt} width={150} height={150} priority={true} />
+			<ProfilePicture>
+				<Image src={photo.url} alt={photo.alt} width={150} height={150} priority={true} />
+			</ProfilePicture>
 			<Username href={`https://instagram.com/${username}`} target="_blank" rel="noopener noreferrer">
-				@{username.replace(/@/g, '')}
+				<Image src="/icons/instagram.svg" alt="Instagram logo" width={32} height={32} priority={true} />
+				<p>{username.replace(/@/g, '')}</p>
 			</Username>
 		</StyledProfile>
 	);
